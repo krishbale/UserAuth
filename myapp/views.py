a91062aabbaec85a74dedb338c84d01d03c5d971
krishbale <sushil.pokharel.7528@gmail.com>
2023-05-22 15:20:30 +0545

    validationforregister

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Feature
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -8,3 +10,25 @@ def index(request):
    
   
     return render (request, 'index.html',{'features':features})
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+    
+        if password == password1:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username Already Used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request,'Password Not the Same')
+            return redirect('register')    
+    return render(request , 'register.html')
