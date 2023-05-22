ef6e996a601c986c6a3fa569900f95fde6a105c1
krishbale <sushil.pokharel.7528@gmail.com>
2023-05-22 16:24:47 +0545

    loginlogout

--- myapp/views.py
+++ myapp/views.py
@@ -32,3 +32,25 @@ def register(request):
             messages.info(request,'Password Not the Same')
             return redirect('register')    
     return render(request , 'register.html')
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/') 
+        else:
+            messages.info(request,"credentials Invalid")
+            return redirect('login')
+    
+
+
+    
+    return render(request,'login.html') 
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+    
+  
