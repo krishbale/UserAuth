8e7d7e4d50cf5575c6df3d629fadacee0f237852
krishbale <sushil.pokharel.7528@gmail.com>
2023-05-22 14:39:45 +0545

    databasemigratin

--- myapp/views.py
+++ myapp/views.py
@@ -4,9 +4,7 @@ from .models import Feature
 # Create your views here.
 
 def index(request):
-    feature1 = Feature()
-    feature1.name = 'Balkrishna'
-    feature1.id = 1
-    feature1.details = 'hello world'
+    features = Feature.objects.all()
+   
   
-    return render (request, 'index.html',{'feature':feature1})
+    return render (request, 'index.html',{'features':features})
