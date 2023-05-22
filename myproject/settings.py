d1dcbb39871bf5b6e9982f949b0afbd78fb50e69
krishbale <sushil.pokharel.7528@gmail.com>
2023-05-22 21:36:25 +0545

    migraation done

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'myproject',
+        'USER':'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'localhost',
+
+
     }
 }
 
